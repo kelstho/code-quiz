2af985e354e71e96560a0fdab505de0552aa185d
Kelly Thompson <ksthompson48@gmail.com>
2020-05-09 18:07:41 -0500

    create variables

---
+++ script.js
@@ -0,0 +1,9 @@
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+var start = document.getElementById("start");
+var timer = document.getElementById("time");
+var questions = document.getElementById("questions");
+var choices = document.getElementById("choices");
+var initials = document.getElementById("initials");
+var submit = document.getElementById("submit");
