3e4cc0b31650173cb580feaee42f7e6c48832213
Kelly Thompson <ksthompson48@gmail.com>
2020-05-09 18:05:04 -0500

    write questions

---
+++ questions.js
@@ -0,0 +1,22 @@
+var questions = [
+  {
+    title: "JSX is utilized by...",
+    choices: ["MongoDB", "React", "MySQL", "CSS"],
+    answer: "React"
+  },
+  {
+    title: "jQuery is used by which language?",
+    choices: ["Python", "C++", "Javascript", "Spanish"],
+    answer: "Javascript"
+  },
+  {
+    title: "A service worker...",
+    choices: ["makes lots of service", "allows a user to download an app", "allows a user to upload an app", "is worthless"],
+    answer: "allows a user to download an app"
+  },
+  {
+    title: "Which language is the oldest?",
+    choices: ["Javascript", "Python", "Ruby", "C"],
+    answer: "C"
+  }
+]
