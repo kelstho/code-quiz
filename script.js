563c60287e6230f6bcc85ffd7bbaba10a89d59e9
Kelly Thompson <ksthompson48@gmail.com>
2020-05-09 19:46:26 -0500

    create init function

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
-var timerId;
+var iQ = 0;
+var remaining = 60;
 var start = document.getElementById("start");
-var timer = document.getElementById("time");
-var questions = document.getElementById("questions");
+var timer = document.getElementById("timer");
+var questionsTitle = document.getElementById("questions");
 var choices = document.getElementById("choices");
-var initials = document.getElementById("initials");
-var submit = document.getElementById("submit");
+var startDiv = document.getElementById("start-div");
+var titleHeader = document.getElementById("question-title");
+
+var timerStart;
+
+function init() {
+  startDiv.setAttribute("class", "hide");
+  gameDiv.removeAttribute("class");
+  timerStart = setInterval(increment, 1000);
+  timer.textContent = remaining;
+  getQuestion();
+}
+
+start.onclick = init;
