25bd987dbb8350b4cdf916b081ab66ca99fe889c
Kelly Thompson <ksthompson48@gmail.com>
2020-05-09 19:48:23 -0500

    create functions + add variables

--- script.js
+++ script.js
@@ -6,8 +6,30 @@ var questionsTitle = document.getElementById("questions");
 var choices = document.getElementById("choices");
 var startDiv = document.getElementById("start-div");
 var titleHeader = document.getElementById("question-title");
+var gameDiv = document.getElementById("game-div");
+var endDiv = document.getElementById("end-div");
+var finalScore = document.getElementById("final");
 
 var timerStart;
+
+function increment() {
+  remaining--;
+  timer.textContent = remaining;
+}
+
+function getQuestion() {
+  var cQ = questions[iQ];
+  titleHeader.textContent = cQ.title;
+  choices.innerHTML = "";
+  cQ.choices.forEach(function (choice, i) {
+    var selection = document.createElement("button");
+    selection.setAttribute("class", "btn btn-primary ml-2");
+    selection.setAttribute("value", choice);
+    selection.textContent = choice;
+    selection.onclick = userSelection;
+    choices.appendChild(selection);
+  })
+}
 
 function init() {
   startDiv.setAttribute("class", "hide");
